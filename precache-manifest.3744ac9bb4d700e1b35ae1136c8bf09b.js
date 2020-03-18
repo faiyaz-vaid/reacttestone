0793bfde2873a0f2f067583188196e50dd184f6a
Faiyaz Vaid <faiyaz.vaid786@gmail.com>
2020-03-18 10:29:34 +0530

    Updates

---
+++ precache-manifest.3744ac9bb4d700e1b35ae1136c8bf09b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b32a57cb0bcf59c9e66cccd2c7d85f1",
+    "url": "/reacttestone/index.html"
+  },
+  {
+    "revision": "7cd3ee5fb47c956c1239",
+    "url": "/reacttestone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6cfc0a96a88e1f06f82",
+    "url": "/reacttestone/static/js/2.3b2a13c9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reacttestone/static/js/2.3b2a13c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd3ee5fb47c956c1239",
+    "url": "/reacttestone/static/js/main.f3cd2f4c.chunk.js"
+  },
+  {
+    "revision": "9e16792f7455204ee241",
+    "url": "/reacttestone/static/js/runtime-main.9945d7ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttestone/static/media/logo.5d5d9eef.svg"
+  }
+]);
